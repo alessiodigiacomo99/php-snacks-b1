0331d951ca05f0d1f15d85c3149c87c557e95936
alessio <alessiodg99.adg@gmail.com>
2021-01-29 16:10:45 +0100

    fine es 2

--- es 2/index.php
+++ es 2/index.php
@@ -1,3 +1,30 @@
+<?php
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+if (strlen($name) > 3 && strpos($mail, "@") !== false && strpos($mail, ".") !== false && is_numeric($age) == true){
+    $var = "Accesso riuscito";
+}else{
+    $var = "Accesso negato";
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1><?php echo $var ?></h1>
+    </body>
+</html>
+
+
+
+
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non
 conosciamo nella documentazione) che:
 1. name sia più lungo di 3 caratteri,
